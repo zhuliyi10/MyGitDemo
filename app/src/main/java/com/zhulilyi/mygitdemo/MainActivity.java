dd60b5d84f128e01bc897d626f5ab729552b026c
zhuliyi10 <2280835569@qq.com>
2018-04-13 21:27:05 +0800

    首次提交

---
+++ app/src/main/java/com/zhulilyi/mygitdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zhulilyi.mygitdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
